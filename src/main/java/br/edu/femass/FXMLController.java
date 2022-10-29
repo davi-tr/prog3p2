c6abf319b62594a56df4022333126ea088c0a1e1
Davi Trajano <davitrajano473@gmail.com>
2022-10-29 02:21:57 -0300

    Primeira Aula

---
+++ src/main/java/br/edu/femass/FXMLController.java
@@ -0,0 +1,35 @@
+package br.edu.femass;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import br.edu.femass.model.Cliente;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class FXMLController implements Initializable {
+    
+    @FXML
+    private TextField TxtNome;
+    
+    @FXML
+    private TextField TxtEndereco;
+
+    @FXML
+    private ListView<Cliente> LstClientes;
+
+    @FXML
+    private void Gravar_Click(ActionEvent event) {
+        Cliente cliente = new Cliente(TxtNome.getText(),TxtEndereco.getText());
+        System.out.println(cliente.getNome());
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
